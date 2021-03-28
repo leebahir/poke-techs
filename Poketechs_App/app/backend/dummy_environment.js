13c7d869c4c78e212a75c11db0e8695fba2ee87e
human-bootleg <73385664+human-bootleg@users.noreply.github.com>
2021-03-27 23:13:59 -0700

    Update dummy_environment.js
    Change key to dummy

--- Poketechs_App/app/backend/dummy_environment.js
+++ Poketechs_App/app/backend/dummy_environment.js
@@ -2,7 +2,7 @@ var Environment = {
     GOOGLE_AUTOML_PROJECT_ID: 'poke-techs',
     GOOGLE_AUTOML_LOCATION: 'us-central1',
     GOOGLE_AUTOML_MODEL_ID: 'ICN7729085157172314112',
-    AUTH_BEARER: 'ya29.c.Kp8B-QcGL71qTK09m-BaMnf4U_vkQR1yv3UJ2JlFuZTa1_NlDMrgpbJlbhZKURA-LdtkODewz3Q6qj-3hr3C7vMI9oVaoICKM67fkFQB1Z62A7b0QuZt-5HCeqJ7cPC8fFsr_9StPAKLbzk2vDHl21Hnr1_6xa9yp1xkRcnaN0oW8c0-kBSRnCfhwJutYHmNeOsvHDk8jhUMRdy0nvJ1xQLy'
+    AUTH_BEARER: 'XXXX'
 }
 
 export default Environment;
